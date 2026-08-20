da0f864826970ad573c02a5cb149a3e70ed11422
Thomas Iles <tomiles@gmail.com>
2026-08-20 11:53:36 +0100

    Show routes for final page
    Show the routes for the final page of the form, if the page is a
    selection page with only one option and less than 10 options.

--- spec/views/routes/show.html.erb_spec.rb
+++ spec/views/routes/show.html.erb_spec.rb
@@ -210,6 +210,50 @@ describe "routes/show.html.erb" do
       end
     end
 
+    context "when the final page is a selection page" do
+      let(:pages) do
+        [
+          build_stubbed(:page, id: 101),
+          build_stubbed(:page, id: 102),
+          build_stubbed(
+            :page,
+            :with_selection_settings,
+            id: 103,
+            routing_conditions: [
+              build_stubbed(
+                :condition,
+                routing_page_id: 103,
+                goto_page_id: 101,
+                answer_value: "Option 1",
+              ),
+            ],
+          ),
+        ]
+      end
+
+      let(:routes_input) do
+        build(:routes_input, form:).assign_form_values.tap do |routes_input|
+          routes_input.routes.each { |route| allow(route).to receive(:invalid?).and_return(true) }
+        end
+      end
+
+      it "shows the selected goto page for the route" do
+        render_page
+
+        expect(rendered).to have_selector('.govuk-select[name="forms_routes_input[routes_attributes][1][goto]"]') do |field|
+          expect(field).to have_selector("option[selected]") do |option|
+            expect(option["value"]).to eq "default"
+          end
+        end
+      end
+
+      it "has a button for adding exit pages" do
+        render_page
+        expect(rendered).to have_button("Add a new exit page")
+        expect(rendered).to have_selector("button[name='new_exit_page'][value='#{pages.last.id}']")
+      end
+    end
+
     context "when a page is a select from a list question" do
       let(:pages) do
         [
